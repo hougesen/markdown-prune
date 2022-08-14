2cf523217a72e5832abb084a2b318f9bae39da61
Mads Hougesen <madshougesen@gmail.com>
2022-08-14 18:06:51 +0200

    refactor: moved travere_dir to directory file

---
+++ src/directory.rs
@@ -0,0 +1,43 @@
+use crate::{check_if_bad_file, delete_file};
+
+#[derive(Debug)]
+pub struct DeleteResult {
+    pub file_count: u16,
+    pub bytes: u64,
+}
+
+pub fn traverse_dir(path: std::path::PathBuf, delete_files: bool) -> std::io::Result<DeleteResult> {
+    let mut result = DeleteResult {
+        file_count: 0,
+        bytes: 0,
+    };
+
+    for entry in std::fs::read_dir(&path)? {
+        let entry_path = entry?.path();
+
+        if entry_path.is_dir() {
+            let path_result = traverse_dir(entry_path, delete_files)?;
+
+            result.file_count += path_result.file_count;
+            result.bytes += path_result.bytes;
+        } else if entry_path.is_file() {
+            let file_extension = entry_path.extension();
+            let bad_file = check_if_bad_file(file_extension);
+
+            if bad_file {
+                result.file_count += 1;
+                let file_size = entry_path.metadata()?.len();
+
+                if file_size > 0 {
+                    result.bytes += file_size;
+                }
+
+                if delete_files {
+                    delete_file(entry_path);
+                }
+            }
+        }
+    }
+
+    Ok(result)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
 mod config;
 mod convert_bytes;
+mod directory;
 
 use config::Config;
 use convert_bytes::{convert_bytes, ByteSize};
+use directory::traverse_dir;
 use std::{ffi::OsStr, path::PathBuf};
-
-#[derive(Debug)]
-struct DeleteResult {
-    file_count: u16,
-    bytes: u64,
-}
 
 fn main() -> std::io::Result<()> {
     let config = Config::new();
@@ -40,42 +36,6 @@ fn main() -> std::io::Result<()> {
     }
 
     Ok(())
-}
-
-fn traverse_dir(path: PathBuf, delete_files: bool) -> std::io::Result<DeleteResult> {
-    let mut result = DeleteResult {
-        file_count: 0,
-        bytes: 0,
-    };
-
-    for entry in std::fs::read_dir(&path)? {
-        let entry_path = entry?.path();
-
-        if entry_path.is_dir() {
-            let path_result = traverse_dir(entry_path, delete_files)?;
-
-            result.file_count += path_result.file_count;
-            result.bytes += path_result.bytes;
-        } else if entry_path.is_file() {
-            let file_extension = entry_path.extension();
-            let bad_file = check_if_bad_file(file_extension);
-
-            if bad_file {
-                result.file_count += 1;
-                let file_size = entry_path.metadata()?.len();
-
-                if file_size > 0 {
-                    result.bytes += file_size;
-                }
-
-                if delete_files {
-                    delete_file(entry_path);
-                }
-            }
-        }
-    }
-
-    Ok(result)
 }
 
 fn check_if_bad_file(file_extension: Option<&OsStr>) -> bool {
