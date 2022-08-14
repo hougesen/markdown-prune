2745fb736bb313d162e19c8f1c47ea1581f3367d
Mads Hougesen <madshougesen@gmail.com>
2022-08-14 18:43:38 +0200

    fix: added support for .mkd & .markdown

--- src/file.rs
+++ src/file.rs
@@ -1,7 +1,9 @@
+const BAD_EXTENSIONS: [&str; 3] = ["md", "markdown", "mkd"];
+
 pub fn check_if_bad_file(file_extension: Option<&std::ffi::OsStr>) -> bool {
     match file_extension {
         Some(ext) => {
-            if ext == "md" {
+            if BAD_EXTENSIONS.contains(&ext.to_str().unwrap_or("")) {
                 return true;
             }
 
