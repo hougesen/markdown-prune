49f0a0beee57df1258ef1b67b3166d48e8438a4a
Mads Hougesen <madshougesen@gmail.com>
2022-08-14 18:33:32 +0200

    feat: support --unit arg

--- src/config.rs
+++ src/config.rs
@@ -1,13 +1,17 @@
+use crate::convert_bytes::{parse_byte_type, ByteSize};
+
 pub struct Config {
     pub path: Option<std::path::PathBuf>,
-    pub delete: bool,
+    pub delete_files: bool,
+    pub unit: ByteSize,
 }
 
 impl Config {
     pub fn new() -> Config {
         let mut config = Config {
             path: None,
-            delete: true,
+            delete_files: true,
+            unit: ByteSize::MB,
         };
 
         let args: Vec<String> = std::env::args().collect();
@@ -19,11 +23,20 @@ impl Config {
 
         for i in 0..args.len() {
             match args[i].as_str() {
-                "--dry" | "--check" => config.delete = false,
+                "--dry" | "--check" => config.delete_files = false,
                 "--path" => {
                     if i + 1 < args.len() {
                         let path = std::path::PathBuf::from(args[i + 1].clone());
                         config.path = Some(path);
+                    } else {
+                        println!("ERROR: --path is missing an input");
+                    }
+                }
+                "--unit" => {
+                    if i + 1 < args.len() {
+                        config.unit = parse_byte_type(&args[i + 1]);
+                    } else {
+                        println!("WARNING: --unit is missing an input");
                     }
                 }
                 _ => continue,
